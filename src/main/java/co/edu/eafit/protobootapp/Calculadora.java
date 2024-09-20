6d11af8774f80262f9ae3e8018386193aadf9b28
dei <dsanche3@eafit.edu.co>
2024-09-20 11:20:28 -0500

    Probando dañar el estilo

--- src/main/java/co/edu/eafit/protobootapp/Calculadora.java
+++ src/main/java/co/edu/eafit/protobootapp/Calculadora.java
@@ -3,11 +3,17 @@ package co.edu.eafit.protobootapp;
 //This is a simple calculator class
 public class Calculadora {
 
-    public Double cuadrado(Double numero) {
-        return numero * numero;
+        public Double cuadrado(Double Numero) {
+            return numero * numero;
     }
     
     public Double cubo(Double numero) {
         return numero * numero * numero;
     }
+
+
+
+
+
+    
 }
